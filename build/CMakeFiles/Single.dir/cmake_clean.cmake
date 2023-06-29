73193f4c6c53dfe03ea432611d10e74d3e19203b
praveen <praveen>
2023-06-29 17:11:43 +0530

    single_linked_list

---
+++ build/CMakeFiles/Single.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../Single"
+  "../Single.pdb"
+  "CMakeFiles/Single.dir/single_linked_list.cpp.o"
+  "CMakeFiles/Single.dir/single_linked_list.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Single.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/T. Praveen Kumar/Pictures/single
+# Build directory: /mnt/c/Users/T. Praveen Kumar/Pictures/single/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
